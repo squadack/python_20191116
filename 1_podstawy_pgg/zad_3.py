fc8141bbe7e1a277139de90b39da300714c9b53c
Piotr Grabski-Gradzinski <piotr.gradzinski@gmail.com>
2019-11-16 14:23:08 +0100

    Zjazd 1 - zad 3

---
+++ 1_podstawy_pgg/zad_3.py
@@ -0,0 +1,19 @@
+"""
+Napisz program wypisujący na konsolę Twoje imię i wzrost. Do
+przechowywania informacji o Twoim imieniu i wzroście użyj
+zmiennych.
+Przykładowy komunikat programu:
+Imię: Jan
+Wzrost: 180
+"""
+
+imie = 'Piotr'
+wzrost = 182
+
+# podajemy JEDEN argument
+print('Imię: ' + imie)
+print('Wzrost: ' + str(wzrost))
+
+# podajemy DWA argumenty
+print('Imię:', imie) # przy kilku argumentach, print oddziela je od siebie spacją
+print('Wzrost:', wzrost)
