91fea402175b88d2e84969ca9ddbd0364dca2f06
Piotr Grabski-Gradzinski <piotr.gradzinski@gmail.com>
2019-11-17 09:31:44 +0100

    Zjazd 1 - zad 12

--- 1_podstawy_pgg/zad_12.py
+++ 1_podstawy_pgg/zad_12.py
@@ -3,3 +3,8 @@ Napisz program obliczający kwadrat 100 pierwszych liczb
 całkowitych i wypisujący wyniki na konsolę.
 """
 
+i = 1
+
+while i <= 100:
+    print(f'Kwadrat liczby {i} to {i**2}')
+    i += 1
