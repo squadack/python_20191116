b2ac11ddcbb88553cb6a954a33c3019529f07a5d
Piotr Grabski-Gradzinski <piotr.gradzinski@gmail.com>
2019-11-17 09:45:25 +0100

    Zjazd 1 - zad 13

--- 1_podstawy_pgg/zad_13.py
+++ 1_podstawy_pgg/zad_13.py
@@ -6,12 +6,13 @@ użytkownika.
 
 suma_temperatur = 0
 numer_dnia = 1
+LICZBA_DNI_TYGODNIA = 7
 
-while numer_dnia <= 7:
-    suma_temperatur += int(input(f"Podaj temperaturę z dnia {numer_dnia}"))
+while numer_dnia <= LICZBA_DNI_TYGODNIA:
+    suma_temperatur += int(input(f"Podaj temperaturę z dnia {numer_dnia}: "))
     numer_dnia += 1
 
-srednia_temperatur = suma_temperatur / 7
+srednia_temperatur = suma_temperatur / LICZBA_DNI_TYGODNIA
 
 print(f"Srednia temperatura w tym tygodniu to {srednia_temperatur}")
 
