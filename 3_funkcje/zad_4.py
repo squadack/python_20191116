bbb3121dcc5ac97d178062a77083164943b7200a
Piotr Grabski-Gradzinski <piotr.gradzinski@gmail.com>
2019-12-01 10:32:23 +0100

    Zjazd 2 - zad 4

--- 3_funkcje/zad_4.py
+++ 3_funkcje/zad_4.py
@@ -19,15 +19,17 @@ def formatuj(*args, **kwargs):
 
     return '\n'.join(rezultat)
 
-print( formatuj('koszt $cena PLN', 'kwota $cena brutto', cena=10) )
-print( formatuj('ala ma $co', co='kota') )
-print( formatuj('Nazywam $imie $nazwisko', imie='Piotr', nazwisko='GG') )
-
 def test_wszystko():
-    assert True
-
-
-
+    assert formatuj('Hello World!') == 'Hello World!'
+    assert formatuj('Hello World!', imie='Piotr') == 'Hello World!'
+    assert formatuj('Ala ma $co', co='kota') == 'Ala ma kota'
+    assert formatuj('$kto najlepsze $co', kto='ALX', co='szkolenia') == 'ALX najlepsze szkolenia'
+    assert formatuj('kwota $cena', 'wartość $cena', cena=10) == 'kwota 10\nwartość 10'
+    assert formatuj(
+        'Autor $imie $nazwisko',
+        'Cześć $imie $nazwisko',
+        imie='Piotr',
+        nazwisko='GG') == 'Autor Piotr GG\nCześć Piotr GG'
 
 
 
