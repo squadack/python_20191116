91f7dcdba934bfe4354596f3ec3f670677a1c3cf
Piotr Grabski-Gradzinski <piotr.gradzinski@gmail.com>
2019-12-01 10:56:18 +0100

    Zjazd 2 - zad 5

--- 3_funkcje/zad_5.py
+++ 3_funkcje/zad_5.py
@@ -10,6 +10,12 @@ def silnia(n: int):
         # return None # programista używający naszej funkcji będzie musiał dodatkowo sprawdzić czy nie dostał None
         raise ValueError('n musi byc intem i być większe od 0')
 
+    wynik = 1
+
+    for i in range(1, n + 1):
+        wynik *= i
+
+    return wynik
 
 import pytest
 
@@ -18,7 +24,12 @@ def test_n_mniejsze_od_zera():
         silnia(-10)
 
 def test_n_zero():
-    pass
+    assert silnia(0) == 1
 
 def test_n_wieksze_od_zero():
-    pass
+    assert silnia(3) == 6
+    assert silnia(4) == 24
+    assert silnia(5) == 120
+    assert silnia(6) == 720
+    assert silnia(7) == 5040
+    assert silnia(8) == 40320
