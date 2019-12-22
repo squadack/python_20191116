f756120a01f027996dc06e94035eff608c41f946
Piotr Grabski-Gradzinski <piotr.gradzinski@gmail.com>
2019-12-22 10:48:47 +0100

    Zjazd 3 - zadanie 6

--- dzien5/oop/zadanie6.py
+++ dzien5/oop/zadanie6.py
@@ -9,13 +9,36 @@ class Vector:
     def __add__(self, other):
         return Vector(self.x + other.x, self.y + other.y)
 
+    def __sub__(self, other):
+        return Vector(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, other): # self * other
+        return Vector(self.x * other, self.y * other)
+
+    def __rmul__(self, other): # other * self
+        return self * other
+
+    def length(self):
+        return (self.x ** 2 + self.y ** 2) ** (1 / 2)
+
+    def __lt__(self, other): # self < other
+        return self.length() < other.length()
+
+    def __eq__(self, other): # self == other
+        return self.x == other.x and self.y == other.y
+
 v1 = Vector(1, 2)
 v2 = Vector(2, 3)
 print(v1)
 print(v2)
-v3 = v1 + v2 # v3 = v1.__add__(v2)
+v3 = 5 * v1
 print(v3)
-print(f"{v1} + {v2} = {v1 + v2}")
+if v1 < v2:
+    print("v1 mniejsze")
+if v1 == Vector(1, 2):
+    print("TAK")
+# a * b # a.__mul__(b) | b.__rmul__(a)
+# print(f"{v1} - {v2} = {v1 - v2}")
 # v1 < v2 # v1.__lt__(v2)
 # v2 == v2 # v1.__eq__(v2)
 # v1 * 3
